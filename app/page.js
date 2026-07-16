e7a2dac8f1496e5ae725be699d092ffe17cb9542
cpurella <chandrashekarpurella@gmail.com>
2026-07-16 20:23:42 +0500

    Delete app directory

--- app/page.js
+++
@@ -1,7 +0,0 @@
-import { getAllItems } from "../lib/data";
-import DashboardClient from "../components/DashboardClient";
-
-export default function HomePage() {
-  const items = getAllItems();
-  return <DashboardClient items={items} />;
-}
