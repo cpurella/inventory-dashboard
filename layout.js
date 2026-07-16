4213d3b0bff75aaedc8cadff5656228a1428c85b
cpurella <chandrashekarpurella@gmail.com>
2026-07-16 20:19:58 +0500

    Delete layout.js

--- layout.js
+++
@@ -1,9 +0,0 @@
-import { getAllItems, getCategories, getLatestActiveMonth } from "../lib/data";
-import DashboardClient from "../components/DashboardClient";
-
-export default function HomePage() {
-  const items = getAllItems();
-  const categories = getCategories();
-  const defaultMonth = getLatestActiveMonth();
-  return <DashboardClient items={items} categories={categories} defaultMonth={defaultMonth} />;
-}
